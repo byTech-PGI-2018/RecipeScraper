6455ef103e8f7b3557cb98e3a9f7d6aa263cedfe
filipao223 <jpmontenegro67@gmail.com>
2020-01-22 21:34:29 +0000

    Added more xpath queries for when recipe page has a different layout.

--- src/recipeScraper/spiders/receitas_portugal.py
+++ src/recipeScraper/spiders/receitas_portugal.py
@@ -1,26 +1,67 @@
 # -*- coding: utf-8 -*-
-import scrapy
 import json
 import re
+import scrapy
 
 # Max: 215
 
 # URL to make AJAX requests
-ajaxUrl = 'http://www.receitasdeportugal.com/?infinity=scrolling'
+AJAX_URL = 'http://www.receitasdeportugal.com/?infinity=scrolling'
 
 # Form data to include in the AJAX request
-ajaxdata = {
+AJAX_DATA = {
     'action': 'infinite_scroll',
     'page': '1',
     'order': 'DESC'
 }
 
+# List of possible xpath queries for ingredients and quantities
+INGREDIENTS_XPATH = [
+    '//*[@class="ingredient-item-name"]',
+    '//*[@class="ingredient"]',
+    '//*[@class="shortcode-ingredients"]/ul/li',
+    '//*[@class="ingredients-list"]/li'
+]
+
+# List of possible xpath queries for preparation steps
+PREPARATION_XPATH = [
+    '//*[@class="direction-step"]',
+    '//*[@class="text_exposed_show"]',
+    '//*[@class="instruction"]',
+    '//*[@class="instructions"]/li',
+    '//*[@class="directions-list"]/li'
+]
+
+
+def get_valid_nodes(response, xpath_list):
+    '''
+    Try to get a non empty list of XML nodes from a selection of xpath
+    queries
+
+    Arguments:
+        response -- XML document
+        xpath_list -- List of strings each representing xpath query
+
+    Returns:
+        None if no XML nodes could be found, and a list of XML nodes
+        otherwise
+    '''
+    for query in xpath_list:
+        elements = response.xpath(query)
+
+        # If list of nodes is not empty
+        if elements.extract():
+            return elements
+
+    return None
+
+
 class ReceitasPortugalSpider(scrapy.Spider):
     name = 'receitas_portugal'
-    #start_urls = ['http://www.receitasdeportugal.com/receitas/']
 
     def __init__(self, **kwargs):
-        # Starting URL, irrelevant, since all links will be extracted from AJAX requests
+        # Starting URL, irrelevant, since all links will be extracted \
+        # from AJAX requests
         self.start_urls = ['http://www.receitasdeportugal.com/receitas/']
 
         super().__init__(**kwargs)
@@ -44,67 +85,88 @@ class ReceitasPortugalSpider(scrapy.Spider):
             self.pagestart = str(int(self.pagestart)+1)
 
             # Change the page in the form data
-            ajaxdata['page'] = self.pagestart
+            AJAX_DATA['page'] = self.pagestart
 
             # Make next AJAX request
-            yield scrapy.FormRequest(ajaxUrl, formdata=ajaxdata, callback=self.parse_ajax)
+            yield scrapy.FormRequest(
+                AJAX_URL, formdata=AJAX_DATA, callback=self.parse_ajax)
 
         else:
             self.logger.info('No more recipes to crawl')
 
-
     def parse_item(self, response):
         # Start making a new dicionary entry
-        newRecipe = {}
+        new_recipe = {}
 
         # Extract recipe name and URL
-        newRecipe['name'] = response.xpath('//*[@class="entry-title fn"]/text()').extract_first()
-        newRecipe['url'] = response.request.url
+        new_recipe['name'] = response\
+            .xpath('//*[@class="entry-title fn"]/text()')\
+            .extract_first()
 
-        # For now, assume these properties don't exist, if they do, they will be updated
-        newRecipe['gastronomia'] = ''
-        newRecipe['tipo'] = ''
-        newRecipe['custo'] = ''
-        newRecipe['calorias'] = ''
-        newRecipe['dificuldade'] = ''
-        newRecipe['tempo'] = ''
-        newRecipe['porção'] = ''
-        newRecipe['vegan'] = False
+        new_recipe['url'] = response.request.url
+
+        # For now, assume these properties don't exist, if they do, they will \
+        # be updated
+        new_recipe['gastronomia'] = ''
+        new_recipe['tipo'] = ''
+        new_recipe['custo'] = ''
+        new_recipe['calorias'] = ''
+        new_recipe['dificuldade'] = ''
+        new_recipe['tempo'] = ''
+        new_recipe['porção'] = ''
+        new_recipe['vegan'] = False
 
         # Try to get dish type
         try:
-            newRecipe['tipo'] = response.xpath('//*[@class="entry-category"]/a[1]/text()').extract_first()
+            new_recipe['tipo'] = response\
+                .xpath('//*[@class="entry-category"]/a[1]/text()') \
+                .extract_first()
         except:
-            self.logger.debug('Recipe has no "dish type" property, in: ' + response.request.url)
+            self.logger.debug('Recipe has no "dish type" property, in: '
+                              + response.request.url)
 
         # Try to check if recipe is vegan or not
         try:
-            vegan = response.xpath('//*[@class="entry-category"]/a[1]/text()').extract_first()
+            vegan = response\
+                .xpath('//*[@class="entry-category"]/a[1]/text()')\
+                .extract_first()
+
             if vegan == 'Vegetariano':
-                newRecipe['vegan'] = True
+                new_recipe['vegan'] = True
         except:
-            self.logger.debug('Recipe has no "vegan" property, in: ' + response.request.url)
+            self.logger.debug('Recipe has no "vegan" property, in: '
+                              + response.request.url)
 
         # Create new dictionaries to hold ingredients and quantities
-        newRecipeIngredients = {}
-        newRecipeQuantities = {}
+        new_recipe_ingredients = {}
+        new_recipe_quantities = {}
 
         # Pattern to discard ingredient unit-related stuff
-        pattern = re.compile('(([0-9]*\.)?[0-9]+(kg|mg|dl|l|ml|g|gr|grs|(colheres)|(colher)|(colh.))?)|^(kg|mg|dl|l|ml|g|gr|grs|(colheres)|(colher)|(colh.))$', re.IGNORECASE) # pylint: disable=anomalous-backslash-in-string
+        pattern = \
+            re.compile('(([0-9]*\.)?[0-9]+(kg|mg|dl|l|ml|g|gr|grs|(colheres)|\
+                (colher)|(colh.))?)|^(kg|mg|dl|l|ml|g|gr|grs|(colheres)|(colher)|\
+                (colh.))$', re.IGNORECASE)  # pylint: disable=anomalous-backslash-in-string
 
         # Get ingredient and quantity info
-        ingredients = response.xpath('//*[@class="ingredients-list"]/descendant::*[@class="ingredient-item"]')
+        ingredients = get_valid_nodes(response, INGREDIENTS_XPATH)
+
+        if ingredients is None:
+            return
 
         # Iterate over all extracted elements
-        for i, ingredientSelector in enumerate(ingredients):
-            # Extract every text node in the <li> tag, also joining any child node text
-            ingredient = ingredientSelector.xpath('normalize-space(string(descendant-or-self::*))').extract_first().strip()
+        for i, ingredient_selector in enumerate(ingredients):
+            # Extract every text node in the <li> tag, also joining \
+            # any child node text
+            ingredient = ingredient_selector\
+                .xpath('normalize-space(string(descendant-or-self::*))')\
+                .extract_first().strip()
 
-            # Since everything is just one string, add it to quantities dictionary
-            newRecipeQuantities[i] = ingredient
+            # Since everything is just one string, add it to \
+            # quantities dictionary
+            new_recipe_quantities[i] = ingredient
 
             # Assume no ingredient was found for current i, for consistency
-            newRecipeIngredients[i] = ''
+            new_recipe_ingredients[i] = ''
 
             # For single ingredients, additional parsing will be needed
             words = ingredient.split()
@@ -112,63 +174,75 @@ class ReceitasPortugalSpider(scrapy.Spider):
             # If the sentence has 'q.b.' anywhere, add every word but 'q.b.'
             if 'q.b.' in words:
                 words.remove('q.b.')
-                newRecipeIngredients[i] = " ".join(words).strip()
-            
+                new_recipe_ingredients[i] = " ".join(words).strip()
+
             # If not, then iterate over every single word
             else:
                 for j, word in enumerate(words):
-                    # If current word matches 'de', just add the rest as ingredient
+                    # If current word matches 'de', just add the \
+                    # rest as ingredient
                     if word and (word in "de"):
-                        newRecipeIngredients[i] = " ".join(words[j+1:]).strip()
+                        new_recipe_ingredients[i] = " ".join(words[j+1:]).strip()
                         break
 
-                    # If the word matches a unit-related pattern (if it's 100ml, 100, 100kg, etc.), continue the loop (ignore it)
+                    # If the word matches a unit-related pattern \
+                    # (if it's 100ml, 100, 100kg, etc.), continue the loop \
+                    # (ignore it)
                     elif word and (bool(pattern.match(word))):
                         pass
 
-                    # If the word doesn't match anything, then just add it and the rest of the words as the ingredient
+                    # If word doesn't match anything, then just add it and \
+                    # the rest of the words as the ingredient
                     else:
-                        newRecipeIngredients[i] = " ".join(words[j:]).strip()
+                        new_recipe_ingredients[i] = " ".join(words[j:]).strip()
                         break
 
         # Add the newly created dictionaries to root dictionary
-        newRecipe['ingredients'] = newRecipeIngredients
-        newRecipe['quantidade'] = newRecipeQuantities
+        new_recipe['ingredients'] = new_recipe_ingredients
+        new_recipe['quantidade'] = new_recipe_quantities
 
         # Create a new dictionary that will hold preparation steps
-        newRecipeInstructions = {}
+        new_recipe_instructions = {}
 
-        # Get all instructions
-        preparation = response.xpath('//*[@class="directions-list"]/descendant::*[@class="direction-step"]')
+        # Get all instructions (only a non empty list of elements)
+        preparation = get_valid_nodes(response, PREPARATION_XPATH)
+
+        if preparation is None:
+            return
 
         # Iterate over every preparation step
         for i, step in enumerate(preparation):
-            newRecipeInstructions[i] = step.xpath('normalize-space(string(descendant-or-self::*))').extract_first()
+            new_recipe_instructions[i] = step\
+                .xpath('normalize-space(string(descendant-or-self::*))')\
+                .extract_first()
 
         # Add preparation dictionary to root dictionary
-        newRecipe['preparação'] = newRecipeInstructions
+        new_recipe['preparação'] = new_recipe_instructions
 
-        yield newRecipe
+        yield new_recipe
 
     def parse(self, response):
-        # Check if we received required arguments (and that they can be parsed with int())
+        # Check if we received required arguments \
+        # (and that they can be parsed with int())
         try:
             if int(self.pagestart) and int(self.pageend):
                 pass
-        except:
-            self.logger.critical('Argument error. Required: -a pagestart=<n> -a pageend=<m>, \
-                                 where n and m are integers n > 0 and m >= n')
+
+        except ValueError:
+            self.logger.critical('Argument error. Not valid integers')
+            return
+
+        except AttributeError:
+            self.logger.critical('Argument error. Required: -a pagestart=<n> -a pageend=<m>')
             return
 
         # Check if pageend is greater or equal than pagestart
-        if int(self.pageend) < int(self.pagestart):
+        if int(self.pageend) < int(self.pagestart):  # pylint: disable=no-member
             self.logger.critical('Ending page cannot be smaller than starting page.')
             return
 
         # Make first AJAX request
-        ajaxdata['page'] = self.pagestart
+        AJAX_DATA['page'] = self.pagestart
 
-        yield scrapy.FormRequest(ajaxUrl, formdata=ajaxdata, callback=self.parse_ajax)
-
-
-
+        yield scrapy.FormRequest(
+            AJAX_URL, formdata=AJAX_DATA, callback=self.parse_ajax)
